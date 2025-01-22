fbf2439aca987c5fe36969c4fa0b64c17e31a0f0
berksarikayaa <berksarikaaya@gmail.com>
2025-01-22 22:47:07 +0300

    Initial Commit

---
+++ JTITask/JTITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JTITaskApp.swift
+//  JTITask
+//
+//  Created by Buket Bayhan on 22.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct JTITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
